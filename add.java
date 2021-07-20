51e87e05192762d2c0dff1a2b60c1b540779759c
Cha-12 <81138379+Cha-12@users.noreply.github.com>
2021-07-20 13:31:38 +0530

    Add files via upload
    introduction

---
+++ add.java
@@ -0,0 +1,20 @@
+package introduction;
+
+public class add {
+
+	public static void main(String[] args) {
+		 System.out.print("The sum of these no is: ");
+		int a,b,c;
+		a=7;
+	    b=8;
+	    c=2;
+	   int sum= a+b+c;	
+	   System.out.println(sum);
+	   
+	   
+		
+		
+
+	}
+
+}
